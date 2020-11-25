8d2dc1eacf58701392df74a74149a403b7efaa25
Angie Spencer <angela.spencer@lastingchangeinc.org>
2020-11-25 18:05:57 -0500

    11-25-20 Filters on users pg

--- dev/private/shared/hr_footer.php
+++ dev/private/shared/hr_footer.php
@@ -11,6 +11,7 @@
  
      <!-- Bootstrap core JavaScript -->
      <script src="<?php echo url_for('/vendor/jquery/jquery.min.js'); ?>"></script>
+     <script src="<?php echo url_for('/vendor/jquery/tableManager.js'); ?>"></script>
     <!-- <script src="https://unpkg.com/@popperjs/core@2"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="<?php echo url_for('vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
--- dev/public/hr/hr_users/index.php
+++ dev/public/hr/hr_users/index.php
@@ -10,7 +10,7 @@
    
     if($result===true){
       $_SESSION['message'] = "User status has been updated.";
-      if($role == '4'){
+      if($role == '4' && $status == '1'){
         $disposition = make_candidate_active($id);
         if($disposition===true){
           $_SESSION['message'] .= " Candidate disposition is now Open.";
@@ -42,12 +42,12 @@
                   <h3 class="m-0 font-weight-bold text-dark text-center">Users</h3>
                 </div> <!-- Card header -->
                 <div class="card-body"> 
-                  <div class="actions text-left mb-2">
-                    <a class="btn" href="<?php echo url_for('/hr/hr_users/new.php') ?>">Create New User</a>
+                <div class="d-flex">  
+                    <button class="btn p-2" type="button" id="toggleInactive">Show Inactive Users</a>
+                    <button class="btn p-2" type="button" id="allUsers">Show All Users</a>
+                    <button class="btn ml-auto p-2" href="<?php echo url_for('/hr/hr_users/new.php') ?>">Create New User</button>
                   </div>
-                  <div>
-                    <button class="btn" type="button" id="toggleInactive">Toggle Inactive</a>
-                  </div>
+                </div>
                 <table
                 id="table"
                 data-toggle="table"
@@ -56,35 +56,42 @@
                 data-search="true">
                 <thead>
                   <tr>
-                    <th style data-sortable="true" data-field="name">Name</th>
-                    <th style data-sortable="true" data-field="email">Email</th>
-                    <th style data-sortable="true" data-field="userType">User Type</th>
-                    <th style data-sortable="true" data-field="status">Status</th>
-                    <th style>Reset Password</th>
+                    <th style data-sortable="true" data-searchable="true" data-field="name">Name</th>
+                    <th style data-sortable="true" data-searchable="true" data-field="email">Email</th>
+                    <th style data-sortable="true" data-searchable="true" data-field="userType">User Type</th>
+                    <th style data-sortable="true" data-searchable="true" data-field="status">Status</th>
+                    <th style></th>
                   </tr>
                 </thead>
                 <tbody>
                   <?php foreach ($user_set as $user) { ?>
-                  <tr class="<?php echo($user['inactive'] == 0 ? 'active' : 'inactive'); ?>">
+                  <tr <?php echo($user['inactive'] == 0 ? '' : 'class="hidden"'); ?>>
                     <td><a class="action" href="<?php echo url_for('/hr/hr_users/edit.php?id=' . h($user['user_id'])); ?>"><?php echo h($user['first_name']) . " " . h($user['last_name']); ?></a></td>
                     <td><?php echo h($user['email']); ?></td>
                     <td><?php echo h($user['role']); ?></td>
+                   
+
                     <td>
-                        <div class="d-inline-block">
+                    <div class="d-inline-block">
                           <p style="color: <?php echo($user['inactive'] == 1 ? "red" : "black"); ?>;"><?php echo($user['inactive'] == 1 ? "Inactive" : "Active"); ?></p>
                         </div>
-
-                        <div class="d-inline-block">
-                          <form  action="<?php echo url_for('/hr/hr_users/index.php?id=' . h(u($user['user_id'])) . '&inactive=' . $user['inactive'] . '&role=' . $user['role_id']); ?>" method="post">
-                            <button type="submit" class="btn btn-primary d-inline" value="submit">Change Status</button>
-                          </form>
-                        </div>
                     </td>
-
                     <td>
+                        <div class="dropdown d-inline-block">
+                        <a type="button" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-v"></i></a>
+                          <div class="dropdown-menu dropdown-primary">
+                          <form  action="<?php echo url_for('/hr/hr_users/index.php?id=' . h(u($user['user_id'])) . '&inactive=' . $user['inactive'] . '&role=' . $user['role_id']); ?>" method="post">
+                            <button type="submit" class="dropdown-item btn btn-dropdown" value="submit">Change Status</button>
+                          </form>
+                        
                       <form onsubmit="return confirm('<?php echo('Would you like to reset the password for ' . h($user['first_name']) . ' ' . h($user['last_name']) . '?'); ?>')" action="<?php echo url_for('/hr/hr_users/delete.php?id=' . h(u($user['user_id']))); ?>" method="post">
-                        <button type="submit" class="btn btn-primary" value="submit">Reset Password</button>
+                        <button type="submit" class="dropdown-item btn btn-dropdown" value="submit">Reset Password</button>
                       </form>
+
+                      
+
+                      </div>
+                      </div>
                     </td>
                   </tr>
                   <?php } ?>
@@ -101,14 +108,23 @@
 <?php include(SHARED_PATH . '/hr_footer.php'); ?>  
 
 <script>
-$(function){
-  $('#table').bootstrapTable()
-})
+// $(function){
+//   $('#table').bootstrapTable()
+// })
+
+// $('.tablemanager').tablemanager();
 
 $(document).ready(function(){
-  $(#toggleInactive).click(function()){
-    $("tr.inactive").toggleClass("hidden");
+  $("#toggleInactive").click(function(){
+    $(this).text($(this).text() == 'Show Active Users' ? 'Show Inactive Users' : 'Show Active Users');
+    $("tbody tr").toggleClass("hidden");
+  });
 });
+
+$(document).ready(function(){
+  $("#allUsers").click(function(){
+    $('tbody tr').show();
+  });
 });
 </script>
 
